acb5efdd7c14b23e0006a09cfd7c120b8bb03bb8
Roukanken <kuko0411@gmail.com>
2025-12-03 22:04:29 +0100

    Day 3: part 1

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -14,8 +14,23 @@ pub fn parse(input: &str) -> IResult<&str, Vec<Vec<u64>>> {
     (parse_input_by_lines(parse_line)).parse(input)
 }
 
+fn highest_joltage(input: &[u64]) -> u64 {
+    let (l, r) = input
+        .into_iter()
+        .enumerate()
+        .fold((0, 0), |(l, r), (index, &x)| match () {
+            _ if x > l && index != input.len() - 1 => (x, 0),
+            _ if x > r => (l, x),
+            _ => (l, r),
+        });
+
+    l * 10 + r
+}
+
 pub fn part_one(input: &str) -> Option<u64> {
-    None
+    let (_, inputs) = parse(input).unwrap();
+
+    Some(inputs.iter().map(|input| highest_joltage(input)).sum())
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
@@ -46,9 +61,18 @@ mod tests {
     }
 
     #[test]
+    fn test_highest_joltage() {
+        let input = vec![8, 1, 8, 1, 8, 1, 9, 1, 1, 1, 1, 2, 1, 1, 1];
+        assert_eq!(highest_joltage(&input), 92);
+
+        let input = vec![1, 2, 3];
+        assert_eq!(highest_joltage(&input), 23);
+    }
+
+    #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(357));
     }
 
     #[test]
